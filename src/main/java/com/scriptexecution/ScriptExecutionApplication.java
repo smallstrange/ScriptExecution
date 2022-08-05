973910b780a2c5329cd84bbf78fa7caca728d973
张解放 <zhangjiefang@s-data.cn>
2022-08-05 10:56:31 +0800

    初始化工程

---
+++ src/main/java/com/scriptexecution/ScriptExecutionApplication.java
@@ -0,0 +1,13 @@
+package com.scriptexecution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptExecutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScriptExecutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scriptexecution/ScriptExecutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scriptexecution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptExecutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
